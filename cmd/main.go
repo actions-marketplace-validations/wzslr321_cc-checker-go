5cfb6bf1e70231298bdbc7dc591be8af29c03a6d
Wiktor Zając <wiktor.zajac888@gmail.com>
2022-10-22 00:00:46 +0200

    list files

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 )
 
-func main() {
+func listFiles() ([]string, error) {
 	workingDirectory := os.Getenv("INPUT_WORKING_DIRECTORY")
 
-	fmt.Sprintf("Working directory: %s", workingDirectory)
+	fileList := []string{}
+	err := filepath.Walk(fmt.Sprintf("./%s", workingDirectory), func(path string, f os.FileInfo, err error) error {
+		if doesFileMatch(path) {
+			fileList = append(fileList, path)
+		}
+		return nil
+	})
+	return fileList, err
+}
+
+func doesFileMatch(path string) bool {
+	if file, err := os.Stat(path); err == nil && !file.IsDir() {
+		return true
+	}
+	return false
+}
+
+func main() {
+	_, err := listFiles()
+	if err != nil {
+		log.Fatal("Fatalny błąd...")
+	}
 }
